8cc29e0ff810e4183fcd73d3a7d2ced7e349d3da
bialger <artur.bigulov@yandex.ru>
2024-02-13 23:25:54 +0300

    Changed Windows installation script, extended gitignore

--- runCMake.bat
+++ runCMake.bat
@@ -1,2 +1,2 @@
 @cd cmake-build && @cd .. && @rmdir /s /q cmake-build
-(cmake -S . -B cmake-build -G "Unix Makefiles" && cmake --build cmake-build && @copy cmake-build\weather-forecast.exe weather-forecast.exe && echo PrintFile installed successfully && .\weather-forecast.exe -h) || echo Something went wrong
+(cmake -S . -B cmake-build -DCMAKE_BUILD_TYPE=Release && cmake --build cmake-build --target weather-forecast && @mklink weather-forecast.exe cmake-build\Debug\weather-forecast.exe && echo weather-forecast installed successfully && .\weather-forecast.exe -h) || echo Something went wrong
