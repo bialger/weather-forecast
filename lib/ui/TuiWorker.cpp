4ccf1fc5f2f82eb2912239d9d12a0da0df4061a0
bialger <artur.bigulov@yandex.ru>
2024-02-20 23:15:22 +0300

    Updated documentation

---
+++ lib/ui/TuiWorker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bialger on 21.02.24.
+//
+
+#include "TuiWorker.h"
---
+++ lib/ui/TuiWorker.hpp
@@ -0,0 +1,12 @@
+//
+// Created by bialger on 21.02.24.
+//
+
+#ifndef WEATHER_FORECAST_LIB_UI_TUIWORKER_H_
+#define WEATHER_FORECAST_LIB_UI_TUIWORKER_H_
+
+class TuiWorker {
+
+};
+
+#endif //WEATHER_FORECAST_LIB_UI_TUIWORKER_H_
