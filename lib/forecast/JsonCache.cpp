43d8e4e5250f1b50d71be9e75abbb3d9359b6d60
bialger <artur.bigulov@yandex.ru>
2024-02-20 13:06:12 +0300

    Implemented JsonCache

--- lib/forecast/JsonCache.cpp
+++ lib/forecast/JsonCache.cpp
@@ -1,1 +1,30 @@
+#include <fstream>
+
 #include "JsonCache.hpp"
+#include "lib/utils/utils.hpp"
+#include "lib/utils/utils.hpp"
+
+JsonCache::JsonCache(const std::string& cache_group, const std::string& common_cache_dir) {
+  cache_group_ = cache_group;
+  cache_dir_ = common_cache_dir + "/" + cache_group_;
+
+  if (!std::filesystem::is_directory(cache_dir_)) {
+    std::filesystem::create_directories(cache_dir_);
+  }
+}
+
+void JsonCache::PutJsonToCache(const std::string& cache_name, const json& data) {
+  std::ofstream cache_file(GetCacheFilename(cache_name).c_str(), std::ios::out | std::ios::binary);
+  std::string json_contents = to_string(data);
+  WriteStringToStream(json_contents, cache_file);
+}
+
+json JsonCache::GetJsonFromCache(const std::string& cache_name) {
+  std::string cache_contents = GetStringFromFile(GetCacheFilename(cache_name));
+  json data = json::parse(cache_name);
+  return data;
+}
+
+std::string JsonCache::GetCacheFilename(const std::string& cache_name) {
+  return cache_dir_ + "/" + cache_name + ".json";
+}
--- lib/forecast/JsonCache.hpp
+++ lib/forecast/JsonCache.hpp
@@ -1,8 +1,24 @@
 #ifndef JSONCACHE_HPP_
 #define JSONCACHE_HPP_
 
-class JsonCache {
+#include <string>
 
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+class JsonCache {
+ public:
+  JsonCache() = delete;
+  JsonCache(const std::string& cache_group, const std::string& cache_dir);
+
+  void PutJsonToCache(const std::string& cache_name, const json& data);
+  json GetJsonFromCache(const std::string& cache_name);
+ private:
+  std::string cache_group_;
+  std::string cache_dir_;
+
+  std::string GetCacheFilename(const std::string& cache_name);
 };
 
 #endif //JSONCACHE_HPP_
